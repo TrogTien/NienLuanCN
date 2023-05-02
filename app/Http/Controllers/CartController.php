ca0367aa86c416f3c9e54a4dda7047bb542cbb85
Tien <trongtien372001@gmail.com>
2023-05-02 21:44:17 +0700

    Them, xoa gio hang

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -8,20 +8,42 @@ use Session;
 
 use App\Http\Requests;
 use Illuminate\Support\Facades\Redirect;
+use Cart;
+
 class CartController extends Controller
 {
     public function save_cart(Request $request) {
 
-        $categorys = DB::table('tbl_category_product')->where('category_status',1)->orderby('category_id','desc')->get();
-        $brands = DB::table('tbl_brand')->where('brand_status',1)->orderby('brand_id','desc')->get();
         
         $product_id = $request->product_id_hidden;
         $quantity = $request->quantity;
 
-        $data = DB::table('tbl_product')->where('product_id',$product_id)->get();
+        $product_info = DB::table('tbl_product')->where('product_id',$product_id)->first();
+        
+
+        $data['id'] = $product_id;
+        $data['qty'] = $quantity;
+        $data['price'] = $product_info->product_price;
+        $data['name'] = $product_info->product_name;
+        $data['weight'] = '1';
+        $data['options']['image'] = $product_info->product_image;
+
+        Cart::add($data);
+        return Redirect::to('show-cart');
+        
+    }
+
+    public function show_cart() {
+        $categorys = DB::table('tbl_category_product')->where('category_status',1)->orderby('category_id','desc')->get();
+        $brands = DB::table('tbl_brand')->where('brand_status',1)->orderby('brand_id','desc')->get();
         
         return view('pages.cart.show_cart')
-            ->with('categorys',$categorys)
-            ->with('brands',$brands);
+        ->with('categorys',$categorys)
+        ->with('brands',$brands);
+    }
+
+    public function delete_to_cart($rowId) {
+        Cart::update($rowId,0);
+        return Redirect::to('/show-cart');
     }
 }
--- resources/views/pages/cart/show_cart.blade.php
+++ resources/views/pages/cart/show_cart.blade.php
@@ -10,11 +10,14 @@
             </ol>
         </div>
         <div class="table-responsive cart_info">
+            <?php
+                $content = Cart::content();
+            ?>
             <table class="table table-condensed">
                 <thead>
                     <tr class="cart_menu">
-                        <td class="name">Tên</td>
-                        <td class="image">Ảnh</td>
+                        <td class="name">Ảnh</td>
+                        <td class="image">Tên</td>
                         <td class="price">Giá</td>
                         <td class="quantity">Số lượng</td>
                         <td class="total">Thành tiền</td>
@@ -22,31 +25,38 @@
                     </tr>
                 </thead>
                 <tbody>
+                    @foreach ($content as $value)
+                        
                     <tr>
                         <td class="cart_product">
-                            <a href=""><img src="images/cart/one.png" alt=""></a>
+                            <a href=""><img src="{{URL::to('public/uploads/product/'.$value->options->image)}}" width="50"  alt=""></a>
                         </td>
                         <td class="cart_description">
-                            <h4><a href="">Colorblock Scuba</a></h4>
-                            <p>Web ID: 1089772</p>
+                            <h4><a href="">{{$value->name}}</a></h4>
                         </td>
                         <td class="cart_price">
-                            <p>$59</p>
+                            <p>{{$value->price}}</p>
                         </td>
                         <td class="cart_quantity">
                             <div class="cart_quantity_button">
                                 <a class="cart_quantity_up" href=""> + </a>
-                                <input class="cart_quantity_input" type="text" name="quantity" value="1" autocomplete="off" size="2">
+                                <input class="cart_quantity_input" type="text" name="quantity" value="{{$value->qty}}" autocomplete="off" size="2">
                                 <a class="cart_quantity_down" href=""> - </a>
                             </div>
                         </td>
                         <td class="cart_total">
-                            <p class="cart_total_price">$59</p>
+                            <p class="cart_total_price">
+                                <?php
+                                    $total = $value->price * $value->qty;
+                                    echo number_format($total).''.'$';
+                                ?>
+                            </p>
                         </td>
                         <td class="cart_delete">
-                            <a class="cart_quantity_delete" href=""><i class="fa fa-times"></i></a>
+                            <a class="cart_quantity_delete" href="{{URL::to('/delete-to-cart/'.$value->rowId)}}"><i class="fa fa-times"></i></a>
                         </td>
                     </tr>
+                    @endforeach
 
                     
                 </tbody>
@@ -54,4 +64,80 @@
         </div>
     </div>
 </section> <!--/#cart_items-->
+<section id="do_action">
+    <div class="container">
+        <div class="heading">
+            <h3>What would you like to do next?</h3>
+            <p>Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.</p>
+        </div>
+        <div class="row">
+            {{-- <div class="col-sm-6">
+                <div class="chose_area">
+                    <ul class="user_option">
+                        <li>
+                            <input type="checkbox">
+                            <label>Use Coupon Code</label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>Use Gift Voucher</label>
+                        </li>
+                        <li>
+                            <input type="checkbox">
+                            <label>Estimate Shipping & Taxes</label>
+                        </li>
+                    </ul>
+                    <ul class="user_info">
+                        <li class="single_field">
+                            <label>Country:</label>
+                            <select>
+                                <option>United States</option>
+                                <option>Bangladesh</option>
+                                <option>UK</option>
+                                <option>India</option>
+                                <option>Pakistan</option>
+                                <option>Ucrane</option>
+                                <option>Canada</option>
+                                <option>Dubai</option>
+                            </select>
+                            
+                        </li>
+                        <li class="single_field">
+                            <label>Region / State:</label>
+                            <select>
+                                <option>Select</option>
+                                <option>Dhaka</option>
+                                <option>London</option>
+                                <option>Dillih</option>
+                                <option>Lahore</option>
+                                <option>Alaska</option>
+                                <option>Canada</option>
+                                <option>Dubai</option>
+                            </select>
+                        
+                        </li>
+                        <li class="single_field zip-field">
+                            <label>Zip Code:</label>
+                            <input type="text">
+                        </li>
+                    </ul>
+                    <a class="btn btn-default update" href="">Get Quotes</a>
+                    <a class="btn btn-default check_out" href="">Continue</a>
+                </div>
+            </div> --}}
+            <div class="col-sm-6">
+                <div class="total_area">
+                    <ul>
+                        <li>Tổng <span>{{Cart::total()}}</span></li>
+                        <li>Thuế <span>{{Cart::tax()}}</span></li>
+                        <li>Phí vận chuyển <span>Free</span></li>
+                        <li>Thành tiền <span>{{Cart::subtotal()}}</span></li>
+                    </ul>
+                        <a class="btn btn-default update" href="">Update</a>
+                        <a class="btn btn-default check_out" href="">Check Out</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section><!--/#do_action-->
 @endsection
