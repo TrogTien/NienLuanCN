5229c7c04044a8d1b4b134498cdaaba4fc70b817
Tien <trongtien372001@gmail.com>
2023-05-08 03:16:22 +0700

    Sua loi liet ke chi tiet don hang

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -142,7 +142,7 @@ class CheckoutController extends Controller
             echo 'Thanh toán khi nhận hàng';
         } 
 
-        return Redirect::to('/payment');
+        return Redirect::to('/checkout-success');
     }
 
     public function manage_order() {
@@ -155,6 +155,7 @@ class CheckoutController extends Controller
         $manager_order = view('admin.manage_order')->with('all_order',$all_order);
         return view('admin_layout')->with('admin.manage_order', $manager_order);
 
+
         
     }
 
@@ -165,7 +166,10 @@ class CheckoutController extends Controller
         ->join('tbl_customer','tbl_customer.customer_id','=','tbl_order.customer_id')
         ->join('tbl_shipping','tbl_shipping.shipping_id','=','tbl_order.shipping_id')
         ->join('tbl_order_details','tbl_order_details.order_id','=','tbl_order.order_id')
+        ->where('tbl_order.order_id', $orderId)
         ->get();
+
+       
 
         $manager_order_by_id = view('admin.view_order')->with('order_by_id',$order_by_id);
         return view('admin_layout')->with('admin.view_order',$manager_order_by_id);
